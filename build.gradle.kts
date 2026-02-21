19dcd531d4eab0152c1e0c4cb852bdff8cbd5095
Dalmeng <dalmenglee@gmail.com>
2026-02-21 16:07:20 +0900

    ✨ Feat: lib update to 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.dalmeng"
-version = "1.0.1"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/dalmeng/common/web/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/dalmeng/common/web/exception/GlobalExceptionHandler.kt
@@ -1,12 +1,19 @@
 package com.dalmeng.common.web.exception
 
 import com.dalmeng.common.web.response.BaseResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.server.ResponseStatusException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
     @ExceptionHandler(BaseException::class)
     fun handleBaseException(e: BaseException): ResponseEntity<BaseResponse<Nothing>> {
@@ -20,14 +27,40 @@ class GlobalExceptionHandler {
             )
     }
 
-    @ExceptionHandler(Exception::class)
-    fun handleUnexpectedException(e: Exception): ResponseEntity<BaseResponse<Nothing>> {
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: org.springframework.http.HttpHeaders,
+        statusCode: org.springframework.http.HttpStatusCode,
+        request: org.springframework.web.context.request.WebRequest
+    ): ResponseEntity<Any>? {
+        val status = HttpStatus.valueOf(statusCode.value())
+        log.error("MVC Exception - ${ex::class.simpleName} - ${ex.message}")
+
         return ResponseEntity
-            .status(500)
+            .status(status)
             .body(
                 BaseResponse.error(
-                    statusCode = 500,
-                    message = e.message ?: e.toString(),
+                    statusCode = status.value(),
+                    message = ex.message ?: "Request error"
+                )
+            )
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpectedException(
+        e: Exception,
+        request: HttpServletRequest
+    ): ResponseEntity<BaseResponse<Nothing>> {
+        val status = HttpStatus.INTERNAL_SERVER_ERROR
+        log.error("Unexpected Exception - ${request.method} ${request.requestURI}", e)
+
+        return ResponseEntity
+            .status(status)
+            .body(
+                BaseResponse.error(
+                    statusCode = status.value(),
+                    message = e.message ?: "Unexpected error"
                 )
             )
     }
